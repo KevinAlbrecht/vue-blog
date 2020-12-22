a1eb6b249605fee354e23d48c990004d461b5957
KevinAlbrecht <bubbuls@gmail.com>
2020-12-23 00:48:03 +0100

    Add custom 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <Layout>
+    <div id="not-found" class="center">
+      <h1>404</h1>
+      <MoonSigh class="center"></MoonSigh>
+      <p></p>
+    </div>
+  </Layout>
+</template>
+<script>
+import MoonSigh from "../components/emoji/MoonSigh";
+export default {
+  components: { MoonSigh },
+};
+</script>
+<style scoped>
+div#not-found h1 {
+    text-align: center;
+}
+
+.center {
+  margin: auto;
+}
+</style>
